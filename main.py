107ea0467bc00f4bffca0a327e8b99c384d14e42
tadashigaki <tadashi.akama@gmail.com>
2013-10-11 02:03:15 +0900

    use webapp framework

--- main.py
+++ main.py
@@ -24,8 +24,22 @@
 #    ('/', MainHandler)
 #], debug=True)
 #
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
 
-print 'Content-Type: text/plain'
-print ''
-print '<p>The time is: %s</p>' % str(datetime.datetime.now())
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        time = datetime.datetime.now()
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write('<p> The time is %s</p>' % str(time))
+
+application = webapp.WSGIApplication([
+        ('/', MainPage)], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
