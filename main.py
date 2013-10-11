8b56840a6a8285dde7c1422386bc7b4b543cc791
tadashigaki <tadashi.akama@gmail.com>
2013-10-12 01:26:41 +0900

    add login function

--- main.py
+++ main.py
@@ -25,6 +25,7 @@
 #], debug=True)
 #
 
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
@@ -32,8 +33,23 @@ import datetime
 class MainPage(webapp.RequestHandler):
     def get(self):
         time = datetime.datetime.now()
+        user = users.get_current_user()
+        if not user:
+            navbar = ('<p>Welcome! <a href="%s">Sign in or register</a> to customize.</p>' % (users.create_login_url(self.request.path)))
+        else:
+            navbar = ('<p>Welcome, %s! Your can <a href="%s">sign out</a>.</p>' % (user.email(), users.create_logout_url(self.request.path)))
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write('<p> The time is %s</p>' % str(time))
+        self.response.out.write('''
+        <html>
+            <head>
+                <title>The time is ...</title>
+            </head>
+            <body>
+            %s
+                <p>The time is: %s</p>
+            </body>
+        </html>
+        ''' % (navbar, str(time)))
 
 application = webapp.WSGIApplication([
         ('/', MainPage)], debug=True)
